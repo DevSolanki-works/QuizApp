36ae253a7df863c9d25b9938cee6bf62eb39f885
Dev <dev.solanki.works@gmail.com>
2026-08-11 19:44:50 +0530

    fix: disable testing mode for AdMob ad units in production

--- forge/frontend/app/admob.js
+++ forge/frontend/app/admob.js
@@ -31,7 +31,7 @@ const RewardedAd = {
   TEST_AD_UNIT_ID: 'ca-app-pub-3940256099942544/5224354917',
   PROD_AD_UNIT_ID: 'ca-app-pub-4922314688440658/3665918421',
 
-  IS_TESTING: true,
+  IS_TESTING: false,
 
   get _adUnitId() {
     return this.IS_TESTING ? this.TEST_AD_UNIT_ID : this.PROD_AD_UNIT_ID;
@@ -140,7 +140,7 @@ const Interstitial = {
   _loading: false,
   TEST_AD_UNIT_ID: 'ca-app-pub-3940256099942544/1033173712',
   PROD_AD_UNIT_ID: 'ca-app-pub-4922314688440658/3840946730',
-  IS_TESTING: true,
+  IS_TESTING: false,
 
   get _adUnitId() {
     return this.IS_TESTING ? this.TEST_AD_UNIT_ID : this.PROD_AD_UNIT_ID;
@@ -201,7 +201,7 @@ const RewardedInterstitial = {
   TEST_AD_UNIT_ID: 'ca-app-pub-3940256099942544/5354046379',
   PROD_AD_UNIT_ID: 'ca-app-pub-4922314688440658/2971130886',
 
-  IS_TESTING: true,
+  IS_TESTING: false,
 
   get _adUnitId() {
     return this.IS_TESTING ? this.TEST_AD_UNIT_ID : this.PROD_AD_UNIT_ID;
